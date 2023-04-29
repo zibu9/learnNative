fd93e3ffd0008e507e75d6f3c175353af48581eb
Zibu <58338897+zibu9@users.noreply.github.com>
2023-04-29 07:37:57 +0100

    add interfaces

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,29 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 
+interface Task {
+    id: number;
+    title: string;
+    isCompleted: boolean;
+}
+
+interface TaskState {
+    tasks: Task[];
+}
+interface Action {
+    type: string;
+    payload: any;
+}
+
+const initialState: TaskState = {
+    tasks: [],
+};
+
+const taskList = () =>{
+    
+}
+  
+
 export const store = configureStore({
     reducer: combineReducers({}),
     devTools:true,
