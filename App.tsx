9df909b8e89648e73c75f6a084113685371583be
Zibu <58338897+zibu9@users.noreply.github.com>
2023-04-29 07:13:31 +0100

    config store redux

--- App.tsx
+++ App.tsx
@@ -9,13 +9,17 @@ import {
   View,
 } from 'react-native';
 import TaskScreen from './src/screens/Tasks/TaskScreen';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.app}>
-      <TaskScreen />
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={styles.app}>
+        <TaskScreen />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
